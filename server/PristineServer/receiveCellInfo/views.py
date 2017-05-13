1b7098a4fc6c4f8c8a4b5ff6a5062fab2de97269
Avikalp Kumar Gupta <t-avgup@microsoft.com>
2017-05-13 07:26:27 +0530

    receiveCellInfo index displays all info in TrackingInfo table. Listener method is accessible on a POST request but currently does not add to database. Track gives TrackingInfo on points from a particular device

--- server/PristineServer/receiveCellInfo/views.py
+++ server/PristineServer/receiveCellInfo/views.py
@@ -5,11 +5,23 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+#from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
+from .models import RawInfo, TrackingInfo
 
 def index(request):
-    return HttpResponse("The visualisation of all the tracked points will appear here!")
+    tracking_points_list = TrackingInfo.objects.order_by('-TimeStamp')
+    #output = ', '.join([(t.Lat, t.Long) for t in tracking_points_list])
+    #eturn HttpResponse("The visualisation of all the tracked points will appear here!<br>" + output)
+
+    #template = loader.get_template('receiveCellInfo/index.html')
+    context = {
+        'tracking_points_list' : tracking_points_list,
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'receiveCellInfo/index.html', context)
 
 def listener(request):
     CID = "5011"
@@ -18,8 +30,28 @@ def listener(request):
     MNC = "861"
     Lat = 10.00
     Long = 1.00
-    return HttpResponse("The location of this cell tower (CID: %s, LAC: %s) is Lat: %f, Long: %f " % (CID, LAC, Lat, Long) )
+
+    try:
+        device = request.POST['device']
+        time_stamp = request.POST['time']
+        CID = request.POST['cid']
+        LAC = request.POST['lac']
+        MCC = request.POST['mcc']
+        MNC = request.POST['mnc']
+        strength = request.POST['dBm']
+    except (KeyError):
+        return render(request, 'receiveCellInfo/listener.html', {
+            'message': "Invalid Raw Information entry requested!"
+        })
+    else:
+        return HttpResponse("The location of this cell tower (CID: %s, LAC: %s) is Lat: %f, Long: %f " % (CID, LAC, Lat, Long) + "<br>Device ID: " + device + "<br>time-stamp: " + time_stamp)
 
 def track(request, device_id):
-    trackedInfo = "<br>coming up"
-    return HttpResponse("Here are the LatLong positions where device " + device_id + ":" + trackedInfo )
+    #trackedInfo = "<br>coming up"
+    #return HttpResponse("Here are the LatLong positions where device " + device_id + ":" + trackedInfo )
+
+    track_points = get_object_or_404(TrackingInfo, deviceID= device_id)
+    context = {'track_points': track_points}
+    return render( request, 'receiveCellInfo/track.html', context)
+
+
